f4db2551a0a6e8dd3fea5064bce561a7bda76059
Rob-Hvx <145124665+Rob-Hvx@users.noreply.github.com>
2023-11-07 09:40:49 +0100

    opdracht 22 begin

--- sprints/sprint 6/StartMap Leerling v1.1/JS/H1/H1O22.js
+++ sprints/sprint 6/StartMap Leerling v1.1/JS/H1/H1O22.js
@@ -19,7 +19,7 @@ function draw() {
 
   text("Beweeg de muis | x = " + round(mouseX) + " en y = " + round(mouseY),10,20);
   
-  if (mouseX >= width - 30) {
+  if (mouseX >= width - 30 || mouseX <= width - 420 || mouseY >= height - 30 || mouseY <= height -420) {
     kleur='red';
   }
   else {
