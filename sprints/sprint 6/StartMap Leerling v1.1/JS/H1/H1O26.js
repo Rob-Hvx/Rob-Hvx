0b66b3abb3512f88cc0f61481a2e037c4db375c5
Rob-Hvx <145124665+Rob-Hvx@users.noreply.github.com>
2023-11-09 17:13:05 +0100

    opdracht 26

--- sprints/sprint 6/StartMap Leerling v1.1/JS/H1/H1O26.js
+++ sprints/sprint 6/StartMap Leerling v1.1/JS/H1/H1O26.js
@@ -9,12 +9,12 @@ function draw() {
   background('orange');
   fill('wheat');
   rect(0,250,width,height - 250);
-  translate(125,0);
+  translate(10,0);
 
-  for (var n = 1;n <= 4;n++) {
+  for (var n = 0;n < 6;n++) {
     tekenHuis();
     tekenBoom(n);
-    translate(200,0);
+    translate(165,0);
   }
 }
 
@@ -35,8 +35,8 @@ function tekenBoom(x) {
   push();
   noStroke();
   fill('sienna');
-  rect(80,150,40,130);
+  rect(80,160,40,130);
   fill('olive');
-  ellipse(100,170 - 15*x,60 + 10*x,100 + 20*x);
+  ellipse(100,180 - 15*x,60 + 10*x,100 + 20*x);
   pop();
 }
