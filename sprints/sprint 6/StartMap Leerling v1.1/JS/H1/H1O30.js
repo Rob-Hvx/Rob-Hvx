0e110b55e2f25e9335a7895dccdf906c5dfd0e9b
Rob-Hvx <145124665+Rob-Hvx@users.noreply.github.com>
2023-11-10 18:05:46 +0100

    opdracht 30

--- sprints/sprint 6/StartMap Leerling v1.1/JS/H1/H1O30.js
+++ sprints/sprint 6/StartMap Leerling v1.1/JS/H1/H1O30.js
@@ -1,4 +1,4 @@
-var rij = 0;
+var kolom = 0;
 
 function setup() {
   canvas = createCanvas(451,451);
@@ -8,7 +8,18 @@ function setup() {
 }
 
 function draw() {
-    for (var kolom = 0;kolom < 450;kolom += 50) {
-      rect(kolom,rij,50,50);
+  for (var rij = 0;rij < 9;rij += 1) {
+    rect(50*kolom,50*rij,50,50);
+
+    for (var kolom = 0;kolom < 9;kolom += 1) {
+      if (rij==3 & kolom==6){
+        fill ('orange');
+        rect(50*kolom,50*rij,50,50);
+      } else {
+        fill('white');
+         rect(50*kolom,50*rij,50,50);
+      }
     }
+  }
+  
 }
