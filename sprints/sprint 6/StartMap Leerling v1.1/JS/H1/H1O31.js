10346e2c5a145cba2c52a1636e1554a000392601
Rob-Hvx <145124665+Rob-Hvx@users.noreply.github.com>
2023-11-11 20:27:08 +0100

    opdracht 31

--- sprints/sprint 6/StartMap Leerling v1.1/JS/H1/H1O31.js
+++ sprints/sprint 6/StartMap Leerling v1.1/JS/H1/H1O31.js
@@ -1,16 +1,28 @@
-kleur='white';
+var kolom = 0;
+var rij = 0
+var kleur = 'black';
+var hoogte = 10;
+var breedte = 10;
 
 function setup() {
-  canvas = createCanvas(451,451);
+  canvas = createCanvas(501,501);
   canvas.parent('processing');
   background('coral');
+  fill('white');
 }
 
 function draw() {
-  for (var rij = 0;rij < 450;rij += 50) {
-    for (var kolom = 0;kolom < 450;kolom += 50) {
-      fill(kleur);
-      rect(kolom,rij,50,50);
+  let hoogteBlokje  = height / hoogte;
+  let breedteBlokje = width / breedte;
+  for (var rij = 0;rij < hoogte;rij += 1) {
+    for (var kolom = 0;kolom < breedte;kolom += 1) {
+      if ((kolom + rij) % 2 == 0){
+        kleur =  'white'
+      } else{
+        kleur = 'black'
+      }
+        fill(kleur);
+        rect(breedteBlokje*kolom,hoogteBlokje*rij,breedteBlokje,hoogteBlokje);
     }
   }
 }
