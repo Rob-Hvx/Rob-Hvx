836577c633f420bc7749aa64050cab317875124b
Rob-Hvx <145124665+Rob-Hvx@users.noreply.github.com>
2023-12-08 16:11:17 +0100

    Hoofdstuk 2 opdracht 2

--- sprints/sprint 6/StartMap Leerling v1.1/JS/H2/H2O02.js
+++ sprints/sprint 6/StartMap Leerling v1.1/JS/H2/H2O02.js
@@ -1,24 +1,26 @@
 var kater,toren,piano;
 
 function preload() {
-  kater = loadImage("images/brieck.jpg");
-
+ kater = loadImage("images/brieck.jpg");
+ toren = loadImage("images/toren.jpg")
 }
 
 function setup() {
-  canvas = createCanvas(850,300);
-  canvas.parent('processing');
-  noLoop();
-  background('grey');
-  fill('white');
-  textFont("Verdana");
-  textSize(14);
-  noStroke();
-  rect(25,25,250,250);
-  rect(300,25,250,250);
+ canvas = createCanvas(850,300);
+ canvas.parent('processing');
+ noLoop();
+ background('grey');
+ fill('white');
+ textFont("Verdana");
+ textSize(14);
+ noStroke();
+ rect(25,25,250,250);
+ rect(300,25,250,250);
 }
 
 function draw() {
-  text("afmeting: " + kater.width + " x " + kater.height,30,20);
-  image(kater,25,25,250,250);
+ text("afmeting: " + kater.width + " x " + kater.height,30,20);
+ image(kater,25,25,250,250);
+ text("afmeting: " + toren.width + " x " + toren.height,300,20);
+ image(toren,300,25,250,250);
 }
