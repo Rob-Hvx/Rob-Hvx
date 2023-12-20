5d5b75f05967a288d02d2ed34dfc43834dfed08b
Rob-Hvx <145124665+Rob-Hvx@users.noreply.github.com>
2023-12-20 16:02:10 +0100

    Hoofdstuk 2 opdracht 8

--- sprints/sprint 6/StartMap Leerling v1.1/JS/H2/H2O08.js
+++ sprints/sprint 6/StartMap Leerling v1.1/JS/H2/H2O08.js
@@ -1,6 +1,6 @@
 var huisNummers = new Array(23,25,27,29,31,33);
 var huisEigenaren = new Array("Smiers","Dekker","Den Hartog","Tolboom","Kremer","Velthuizen");
-var kleur = 'lightgray';
+var kleur;
 
 function setup() {
   canvas = createCanvas(1000,300);
@@ -14,13 +14,19 @@ function setup() {
 function draw() {
   tekenAchtergrond();
 
-  
-    tekenHuis(kleur,13);
+  for (var teller =0;teller < huisNummers.length;teller++){
+    if (teller % 2 === 0) {
+      kleur = 'lightgray';
+    } else {
+      kleur = 'darkgray';
+    }
+    tekenHuis(kleur,huisNummers[teller],teller);
     translate(150,0);
+  }
 
 }
 
-function tekenHuis(kleur,nr) {
+function tekenHuis(kleur,nr,bewoner) {
   push();
   fill(kleur);
   rect(0,110,150,150);
@@ -32,7 +38,7 @@ function tekenHuis(kleur,nr) {
   text(nr,75,190);
   fill('black');
   textSize(15);
-  text("naam",20,165);
+  text(huisEigenaren[bewoner],20,165);
   pop();
 }
 
