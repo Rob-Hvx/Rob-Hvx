7630727fa3294c2958fedda94c3893d07ff69f26
Rob-Hvx <145124665+Rob-Hvx@users.noreply.github.com>
2023-12-20 22:42:17 +0100

    Hoofdstuk 2 opdracht 10

--- sprints/sprint 6/StartMap Leerling v1.1/JS/H2/H2O10.js
+++ sprints/sprint 6/StartMap Leerling v1.1/JS/H2/H2O10.js
@@ -1,32 +1,43 @@
 var animatie = [];
-var aantalBeeldjes = 6;
-var nummer = 0;
+var aantalBeeldjes = 3;
+var nummer = 1;
+var richting = 1;
+var volgendeNummer = 0;
 
 function preload() {
-  for (var b = 0;b < aantalBeeldjes;b++) {
-    nieuw_beeldje = loadImage("images/sprites/Jos_losse_beeldjes/Jos-" + b +".png");
-    animatie.push(nieuw_beeldje);
-  }
-}
+ for (var b = 0;b < aantalBeeldjes;b++) {
+  nieuw_beeldje = loadImage("images/sprites/Jos_losse_beeldjes/Jos-" + b +".png");
+  animatie.push(nieuw_beeldje);
+ }
 
+}
 function setup() {
-  canvas = createCanvas(460,460);
-  canvas.parent('processing');
-  noStroke();
-  frameRate(2);
-  textFont("Georgia");
-  textSize(18);
+ canvas = createCanvas(460,460);
+ canvas.parent('processing');
+ noStroke();
+ frameRate(5);
+ textFont("Georgia");
+ textSize(18);
 }
 
 function draw() {
   background('lavender');
-  image(animatie[nummer],0,0);
-  nummer++;
-
-  if (nummer == aantalBeeldjes) {
-    nummer = 0;
-  }
-  
+  image(animatie[nummer],80,160,300,300);
   text("frameCount=" + frameCount,5,20);
   text("nummer=" + nummer,5,40);
+  if (frameCount % 2 ==0) {
+  nummer = volgendeNummer; 
+  veranderNummer()
+ } else {
+  nummer =2;
+ }
+}
+
+
+function veranderNummer() {
+  if (nummer === 0) {
+    volgendeNummer = 1;
+  } else if (nummer === 1) {
+    volgendeNummer = 0;
+  }
 }
