9640eed0af59b5ce801d540ccd3fb85bb7b92342
Rob-Hvx <145124665+Rob-Hvx@users.noreply.github.com>
2023-12-22 16:30:00 +0100

    Hoofdstuk 2 opdracht 16

--- sprints/sprint 6/StartMap Leerling v1.1/JS/H2/H2O16.js
+++ sprints/sprint 6/StartMap Leerling v1.1/JS/H2/H2O16.js
@@ -12,6 +12,8 @@ var br;
 var ho; 
 var x = 190; 
 var y = 100;
+var stapGroote = 5;
+var aantalBewegingen = 0;
 
 function preload() {
   spriteSheet = loadImage("images/sprites/Pony.png");
@@ -29,10 +31,32 @@ function setup() {
   sBr = breedte / aantalSpriteKolommen;
   sHo = hoogte / aantalSpriteRijen;
   br = sBr*schaal;
-  ho = sBr*schaal;
+  ho = sHo*schaal;
 }
 
 function draw() {
+  if (keyIsDown(RIGHT_ARROW)) {
+    x += stapGroote;
+    rij = 2;
+    aantalBewegingen++;
+  }
+  if (keyIsDown(LEFT_ARROW)) {
+    x -= stapGroote;
+    rij = 1;
+    aantalBewegingen++;
+  }
+  if (keyIsDown(UP_ARROW)) {
+    y -= stapGroote;
+    rij = 3;
+    aantalBewegingen++;
+  }
+  if (keyIsDown(DOWN_ARROW)) {
+    y += stapGroote;
+    rij = 0;
+    aantalBewegingen++;
+  }
+  x = constrain(x,0,width - br);
+  y = constrain(y,0,height - ho);
   background('lavender');
-  image(spriteSheet,x,y,br,ho,(frameCount % aantalSpriteKolommen)*sBr,rij*sHo,sBr,sHo);
+  image(spriteSheet,x,y,br,ho,(aantalBewegingen % aantalSpriteKolommen)*sBr,rij*sHo,sBr,sHo);
 }
